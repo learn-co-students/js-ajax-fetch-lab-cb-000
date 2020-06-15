2a7526c40c8a9aec0635705f3b8149c87174dc4d
s-blais <stephen@s-blais.com>
2020-06-15 16:37:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 's-blais';
 
 function getToken() {
   //change to your token to run in browser, but set
@@ -9,17 +9,60 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  // POST /repos/:owner/:repo/forks
+  fetch(`${baseURL}/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+  .then(resp => resp.json())
+  .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  
+  document.getElementById("results").innerHTML += `<a href="${json.html_url}">${json.html_url}</a>`;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const issueData = {
+    title: document.getElementById("title").value,
+    body: document.getElementById("body").value
+  };
+  fetch(
+    `${baseURL}/repos/${user}/js-ajax-fetch-lab/issues`,
+    {
+      method: 'POST',
+      body: JSON.stringify(issueData),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+  .then(response => response.json())
+  .then(json => getIssues()); // why does getIssues need an argument to work? It works fine with no argument! Does .then require an argument on its callback even if it doesn't use it? Don't know enough about Promise yet.
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  // GET /repos/:owner/:repo/issues
+  fetch(
+    `${baseURL}/repos/${user}/js-ajax-fetch-lab/issues`,
+    {
+      headers: {
+      Authorization: `token ${getToken()}`
+      }
+    }
+  )
+  .then(response => response.json())
+  .then(json => issuesHTML(json));
+}
+
+function issuesHTML(issues) {
+  let result = issues.map((issue)=>renderIssue(issue)).join('');
+  document.getElementById("issues").innerHTML += `<ul>${result}</ul>`;
+}
+
+function renderIssue(issue) {
+  return`<li>${issue.title}</li>`;
 }
